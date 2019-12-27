e1b26fdaf09ff3729d07ed7ee3192c89c6bae397
Hosek Na <hsna7024@gmail.com>
2019-12-27 16:01:24 +0900

    refactor: Update userDetail

--- controllers/userController.js
+++ controllers/userController.js
@@ -69,6 +69,11 @@ export const logout = (req, res) => {
   req.logout();
   res.redirect(routes.home);
 };
+
+export const getMe = (req, res) => {
+  res.render("userDetail", { pageTitle: "User Detail", user: req.user });
+};
+
 export const userDetail = (req, res) => res.render("userDetail");
 export const editProfile = (req, res) => res.render("editProfile");
 export const changePassword = (req, res) => res.render("changePassword");
