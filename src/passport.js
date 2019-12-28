2c48a79169b39da12764be0f543bb529590cc89f
Hosek Na <hsna7024@gmail.com>
2019-12-29 01:34:33 +0900

    refactor: Modify callbackURL in github passport

--- src/passport.js
+++ src/passport.js
@@ -15,7 +15,9 @@ passport.use(
     {
       clientID: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`
+      callbackURL: process.env.PRODUCTION
+        ? `https://immense-scrubland-37555.herokuapp.com/${routes.githubCallback}`
+        : `http://localhost:4000${routes.githubCallback}`
     },
     githubLoginCallback
   )
