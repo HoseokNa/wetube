d6683c5f7f237203972a9c130f876c9e75b5b494
Hosek Na <hsna7024@gmail.com>
2019-12-31 21:01:46 +0900

    refactor: Modify app url

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const url = {
   LOCAL_URL: "http://localhost:4000",
-  PRODUCT_URL: "https://immense-scrubland-37555.herokuapp.com"
+  PRODUCT_URL: "https://ho-wetube.herokuapp.com/"
 };
 
 export const flashMessage = {
