e611d98e8f2b0985b00cb6fc30a2c5630c6a6d68
Hosek Na <hsna7024@gmail.com>
2020-01-03 15:57:52 +0900

    refactor: Add import in root js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,8 @@
 import "../scss/styles.scss";
 import App from "./App"
+import VideoPlayer from "./VideoPlayer";
+import VideoRecorder from "./VideoRecorder";
+import Comment from "./Comment";
+import CommentInput from "./CommentInput";
 
 const app = new App();
