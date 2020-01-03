62b6af651fdcf555b8a436f3269fdb5c440e2836
Hosek Na <hsna7024@gmail.com>
2020-01-03 16:21:57 +0900

    Revert "refactor: Add import in root js"
    This reverts commit e611d98e8f2b0985b00cb6fc30a2c5630c6a6d68.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,4 @@
 import "../scss/styles.scss";
 import App from "./App"
-import VideoPlayer from "./VideoPlayer";
-import VideoRecorder from "./VideoRecorder";
-import Comment from "./Comment";
-import CommentInput from "./CommentInput";
 
 const app = new App();
