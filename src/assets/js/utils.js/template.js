95305a0beb32b1e06b875324f78ba58f1e714d90
Hosek Na <hsna7024@gmail.com>
2020-01-22 17:25:26 +0900

    refactor: Modify comment template

--- src/assets/js/utils.js/template.js
+++ src/assets/js/utils.js/template.js
@@ -7,12 +7,12 @@ export const reCommentTemplate = (loggedUserId, reComment, index) => {
         <div class="comment__contents">
           <div class="contents__creator">${reComment.creator.name}</div>
           <div class="contents__text">${reComment.text}</div>
-          ${
-            loggedUserId === reComment.creator._id
-              ? `<button class="comment__remove">❌</button>`
-              : ``
-          }
         </div>
+        ${
+          loggedUserId === reComment.creator._id
+            ? `<button class="comment__remove">❌</button>`
+            : ``
+        }
       </div>
     </li>`;
 
